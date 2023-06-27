025ee1fba1dd00ba2bb8919780398deccd4ed996
Macumba45 <gonzalolovo@gmail.com>
2023-06-27 15:16:12 +0200

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    experimental: {
-        appDir: true,
-    },
-    compiler: {
-        styledComponents: true,
-    },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
