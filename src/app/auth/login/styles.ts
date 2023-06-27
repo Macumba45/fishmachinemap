8f99f6e208cce185aace2689d1052df5900f7cdb
Macumba45 <gonzalolovo@gmail.com>
2023-06-27 11:02:53 +0200

    initial commit

---
+++ src/app/auth/login/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Container = styled.div``
+
+export const MainContainer = styled.header`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    overflow: hidden;
+    font-family: ${({ theme }) => theme.fonts.poppins};
+`
---
+++ src/app/map/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { FC, memo } from 'react'
+import { Container } from '../auth/login/styles'
+
+const Map: FC = () => {
+    return (
+        <Container>
+            <h1>ESTO ES EL MAPA</h1>
+        </Container>
+    )
+}
+
+export default memo(Map)
