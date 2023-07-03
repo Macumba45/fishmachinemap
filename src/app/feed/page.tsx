9abe001438f9ef4e5c3abb39698fadd8e9a5d509
Macumba45 <gonzalolovo@gmail.com>
2023-07-03 12:42:01 +0200

    refactor: useLogicMaps

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,43 +1,33 @@
 'use client'
 
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
 import { CardContainer, MainContainer } from './style'
 import SimpleBottomNavigation from '@/components/BottomNav'
 import CardList from '@/components/CardList'
-import { shopsListID } from './data'
+import { shopsListID, objetosFeed } from './data'
+import CardFeed from '@/components/CardFeed'
+import { useScrollBlock } from '@/hooks'
 
-const List: FC = () => {
+const Feed: FC = () => {
+
+
+
+
     return (
         <>
             <MainContainer>
-                {shopsListID.map(
-                    ({
-                        id,
-                        title,
-                        description,
-                        image,
-                        titleImage,
-                        city,
-                        address,
-                    }) => (
-                        <CardContainer key={id}>
-                            <CardList
-                                id={id}
-                                title={title}
-                                description={description}
-                                image={image}
-                                titleImage={titleImage}
-                                city={city}
-                                address={address}
-                            />
-                        </CardContainer>
-                    )
-                )}
-
-                <SimpleBottomNavigation />
+                <CardFeed />
+                <CardFeed />
+                <CardFeed />
+                <CardFeed />
+                <CardFeed />
+                <CardFeed />
+                <CardFeed />
             </MainContainer>
+            <SimpleBottomNavigation />
+
         </>
     )
 }
 
-export default List
+export default Feed
