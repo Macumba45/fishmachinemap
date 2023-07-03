7507eb99f2511946e4d013fd5165393ebe9fe082
Macumba45 <gonzalolovo@gmail.com>
2023-07-03 19:26:07 +0200

    feat: process.env.api_key

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+// webpack.config.js
+
+const webpack = require('webpack');
+
+module.exports = {
+    // Configuraciones de tu proyecto Webpack
+
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
+        }),
+    ],
+};
