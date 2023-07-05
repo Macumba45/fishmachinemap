575958e3d0a30da76acc836cd855981886fb889d
Macumba45 <gonzalolovo@gmail.com>
2023-07-05 12:07:44 +0200

    styles: rating added

---
+++ src/components/Rating/index.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Rating from '@mui/material/Rating';
+import Typography from '@mui/material/Typography';
+import { FC } from 'react';
+
+interface Props {
+    value?: number
+}
+
+const BasicRating: FC<Props> = ({ value }) => {
+
+    return (
+        <Box
+            sx={{
+                marginTop: '10px',
+                '& > legend': { mt: 2 },
+            }}
+        >
+
+            <Rating name="read-only" value={value} readOnly />
+        </Box>
+    );
+}
+
+export default BasicRating;
