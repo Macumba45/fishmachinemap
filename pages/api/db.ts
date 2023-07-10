7caed190e9e4122c392e0e54fb27ffaa8d02bc52
Macumba45 <gonzalolovo@gmail.com>
2023-07-10 21:59:47 +0200

    feat:signup doing it

--- pages/api/db.ts
+++ pages/api/db.ts
@@ -5,12 +5,12 @@ declare const global: Global & { prisma?: PrismaClient };
 export let prisma: PrismaClient;
 
 if (typeof window === 'undefined') {
-  if (process.env['NODE_ENV'] === 'production') {
-    prisma = new PrismaClient();
-  } else {
-    if (!global.prisma) {
-      global.prisma = new PrismaClient();
+    if (process.env['NODE_ENV'] === 'production') {
+        prisma = new PrismaClient();
+    } else {
+        if (!global.prisma) {
+            global.prisma = new PrismaClient();
+        }
+        prisma = global.prisma;
     }
-    prisma = global.prisma;
-  }
 }
