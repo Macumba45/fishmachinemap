bf2ff91da067f1da4af436cc0a110c11996a8cd0
Macumba45 <gonzalolovo@gmail.com>
2023-07-11 17:54:28 +0200

    feat: signup and login done

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -13,7 +13,7 @@ import Typography from '@mui/material/Typography'
 import Container from '@mui/material/Container'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
 import { useRouter } from 'next/navigation'
-import { setAuthenticatedToken } from '../../../../pages/storage/storage'
+import { setAuthenticatedToken } from '../../lib/storage/storage'
 import { toast } from 'react-toastify'
 
 // TODO remove, this demo shouldn't need to reset the theme.
