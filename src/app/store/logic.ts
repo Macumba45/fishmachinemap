037d8ffd3ee92adaf54a96205e9430fbacbcd969
Macumba45 <gonzalolovo@gmail.com>
2023-08-02 16:46:59 +0200

    style: new styles in navBar Bottom

---
+++ src/app/store/logic.ts
@@ -0,0 +1,7 @@
+import { User } from 'next-auth'
+import { useState } from 'react'
+import { getAuthenticatedToken } from '../../lib/storage/storage'
+
+export const useLogicUser = () => {
+    return {}
+}
