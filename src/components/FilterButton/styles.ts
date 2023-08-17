f435cbcce4b0d9ffc17767090cab2c6b654837f0
Macumba45 <gonzalolovo@gmail.com>
2023-08-17 11:14:31 +0200

    feat: filter likes user in maps

--- src/components/FilterButton/styles.ts
+++ src/components/FilterButton/styles.ts
@@ -3,9 +3,9 @@ import styled from 'styled-components'
 export const ButtonContainer = styled.div`
     display: flex;
     position: fixed;
-    bottom: 70px;
+    top: 70px;
     align-items: center;
     border-radius: 100px;
     z-index: 99999999;
-    margin-left: 1rem;
+    margin-left: 1.3rem;
 `
