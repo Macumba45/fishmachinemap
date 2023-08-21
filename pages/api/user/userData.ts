72afeacdc7db06c678782c4a836adc5bcfae85ca
Macumba45 <gonzalolovo@gmail.com>
2023-08-21 20:00:03 +0200

    refactor: refactoring api userData

--- pages/api/user/userData.ts
+++ pages/api/user/userData.ts
@@ -1,20 +1,24 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import jwt, { JwtPayload } from 'jsonwebtoken'
 import { getUserInfo } from '../controllers/user'
+import { getAuthenticatedToken } from '@/lib/storage/storage'
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
     if (req.method === 'GET') {
         try {
-            const token = req.headers.authorization?.split(' ')[1] // Obtener el token del encabezado de autorización
+            const token = getAuthenticatedToken()
             if (!token) {
-                throw new Error('Token de autenticación no proporcionado')
+                return res.status(401).json({ message: 'Se requiere un token de autenticación' })
             }
             const decodedToken = jwt.verify(token, 'token') as JwtPayload // Decodificar el token y especificar el tipo como JwtPayload
             const userId = decodedToken.userId // Obtener el ID del usuario desde el token decodificado
             const user = await getUserInfo(userId)
+            console.log('user', user)
             if (!user) {
+                console.log('user', user)
+
                 throw new Error('No se encontró el usuario')
             }
             res.status(200).json({ user })
