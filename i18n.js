2ec5d06b6d7e650e35ecb740f88a1c439c7cf320
Macumba45 <gonzalolovo@gmail.com>
2023-08-30 12:27:14 +0200

    fixing: next-translate in producction

---
+++ i18n.js
@@ -0,0 +1,10 @@
+module.exports = {
+    locales: ['en', 'es'],
+    defaultLocale: 'es',
+    localeDetection: true,
+    pages: {
+        '*': ['common'],
+        // '/': ['home'], // app/page.tsx
+        // '/checkout': ['checkout'], // app/checkout/page.tsx
+    },
+}
