32ca62a601f5663b706052e8f63e29a6e5c96b85
Macumba45 <gonzalolovo@gmail.com>
2023-08-31 23:17:11 +0200

    fix: still fixing translate app

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,18 @@ module.exports = {
             'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
         }),
     ],
-    devtool: false,
-    
+    i18nConfig: {
+        locales: ['es', 'en'],
+        defaultLocale: 'es',
+        pages: {
+            "*": ['common'],
+            "/": ['common'],
+            "/maps": ['common'],
+        },
+        loadLocaleFrom: (lang, ns) =>
+            // You can use a dynamic import, fetch, whatever. You should
+            // return a Promise with the JSON file.
+            import(`./public/locales/${lang}/${ns}.json`).then((m) => m.default)
+    }
+
 }
