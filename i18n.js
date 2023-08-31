4a0e7ef24bbedb2a9fc427ebd95411ad7cb34c29
Macumba45 <gonzalolovo@gmail.com>
2023-08-31 17:18:33 +0200

    fix: still fixing translation

--- i18n.js
+++ i18n.js
@@ -1,8 +1,4 @@
 module.exports = {
-    loadLocaleFrom: (lang, ns) =>
-        // You can use a dynamic import, fetch, whatever. You should
-        // return a Promise with the JSON file.
-        import(`./src/components/locales/${lang}/${ns}.json`).then((m) => m.default),
     locales: ['es', 'en'],
     defaultLocale: 'es',
     pages: {
