16e2c7cf98b8f5fc792748c32bd9d0496ac46440
Macumba45 <gonzalolovo@gmail.com>
2023-09-03 17:36:57 +0200

    feat: home function page redirect if token

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,7 @@
 'use client'
-import { getAuthenticatedToken } from '@/lib/storage/storage'
-import { useLocale } from 'next-intl'
 import { redirect } from 'next/navigation'
 
 export default function Home() {
-    const token = getAuthenticatedToken()
-    if (typeof window !== 'undefined' && typeof navigator !== 'undefined' && typeof navigator.language !== 'undefined') {
-        const locale = navigator.language === 'es' ? 'es' : 'en'
-        if (token) {
-            return redirect(`/${locale}/maps`)
-        } else {
-            return redirect(locale)
-        }
-    }
+
+    return redirect('/es')
 }
