6f5ae34d12c789abc2c22feb2981178d382c1f84
Macumba45 <gonzalolovo@gmail.com>
2023-09-26 12:49:36 +0200

    image web

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -43,7 +43,7 @@ export default async function LocaleLayout({
                 <meta
                     name="description"
                     content="FishGram - APP. ¡Dale caña al carrete!"
-                /> 
+                />
                 <meta
                     property="og:title"
                     content="FishGram - APP. ¡Dale caña al carrete!"
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,18 @@
 import { ReactNode } from 'react'
 import { Metadata } from 'next'
 
-
 type Props = {
     children: ReactNode
 }
 
 export const metadata = {
     title: 'FishGram - APP',
-    description: 'FishGram - APP. ¡Comparte tus spots favoritos, capturas y mucho más!',
+    description:
+        'FishGram - APP. ¡Comparte tus spots favoritos, capturas y mucho más!',
     keywords:
         'pesca, pescadores, pesca deportiva, pesca submarina, tienda de pesca, articulos de pesca',
     applicationName: 'FishGram - APP',
-    images: 'https://i.postimg.cc/W4tdJ1S8/LOGOTIPO-HORIZONTAL-COLOR.png'
-    
-    
-
+    images: 'https://i.postimg.cc/W4tdJ1S8/LOGOTIPO-HORIZONTAL-COLOR.png',
 }
 
 // Since we have a `not-found.tsx` page on the root, a layout file
