933fb778693b231ca1ac020180b134cd0391bd12
Macumba45 <gonzalolovo@gmail.com>
2023-09-28 16:44:27 +0200

    SEO: new config for SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,24 @@ export const metadata = {
         'FishGram te permite encontrar los mejores spots de pesca, compartir tus capturas y conectarte con otros pescadores apasionados. ¡Descubre una comunidad de pesca en línea!',
     keywords:
         'pesca, pescadores, pesca deportiva, pesca submarina, tienda de pesca, articulos de pesca',
+
+    openGraph: {
+        title: 'FishGram - Tus Spots | Tus Capturas. Tu pasión. ¡Buena pesca!',
+        description:
+            'FishGram te permite encontrar los mejores spots de pesca, compartir tus capturas y conectarte con otros pescadores apasionados. ¡Descubre una comunidad de pesca en línea!',
+        url: 'https://fishgramapp.vercel.app/',
+        siteName: 'FisgGram - APP',
+        images: [
+            {
+                url: './opengraph-image.png',
+                width: 800,
+                height: 600,
+                alt: 'FishGram - Logo',
+            },
+        ],
+        locale: 'es_ES',
+        type: 'website',
+    },
 }
 
 // Since we have a `not-found.tsx` page on the root, a layout file
